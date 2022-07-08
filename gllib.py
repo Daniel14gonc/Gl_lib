8e82265064b01309a9016650dc12acd6f00eebed
Daniel <gon20293@uvg.edu.gt>
2022-07-07 18:27:14 -0600

    Initial commit

---
+++ gllib.py
@@ -0,0 +1,24 @@
+
+
+def gitInit():
+
+
+def glCreateWindow(width, height):
+
+
+def glViewPort(x, y, widht, height):
+
+
+def glClear():
+
+
+def glCLearCOlor(r, g, b):
+
+
+def glVertex(x, y):
+
+
+def glColor(r, g, b):
+
+
+def glFinish():
